765a06f1dd8d6c81a282efb790757f1b922e454c
Samisahariar <samisahariar69@gmail.com>
2024-04-28 01:57:26 -0700

    almost done the functionality

--- index.js
+++ index.js
@@ -44,11 +44,25 @@ async function run() {
         })
 
         app.get('/:email', async(req, res) =>{
-            const emails = req.params.email
+            const emails = req.params.email;
             const cursor =  subcategoryCollection.find(); 
             const data = await cursor.toArray()
             const userData = data.filter(singleObject => singleObject.email === emails)
             res.send(userData)
+        })
+
+        app.get('/user/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const singledata = await subcategoryCollection.findOne(query)
+            res.send(singledata)
+        })
+
+        app.put('/del/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await subcategoryCollection.deleteOne(query);
+            res.send(result)
         })
 
         
